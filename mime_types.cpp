e45a4f78598619fa269fc78adf75f7a4f1feffb8
Yukirin <ldy.nero@gmail.com>
2019-02-08 12:45:13 +0900

    Implement mime_types

---
+++ mime_types.cpp
@@ -0,0 +1,41 @@
+//
+// Created by SashiRin on 2019-02-08.
+//
+
+#include "mime_types.hpp"
+#include <vector>
+
+namespace sashi_tiny_http {
+    namespace mime_types {
+        struct MappingItem {
+            const string extension;
+            const string mime_type;
+        };
+
+        vector<MappingItem> mapping_table = {
+            {"html", "text/html"},
+            {"htm", "text/html"},
+            {"php", "text/html"},
+            {"css", "text/css"},
+            {"txt", "text/plain"},
+            {"js", "application/javascript"},
+            {"json", "application/json"},
+            {"xml", "application/xml"},
+            {"gif", "image/gif"},
+            {"jpg", "image/jpeg"},
+            {"jpeg", "image/jpeg"},
+            {"png", "image/png"},
+            {"svg", "image/svg+xml"},
+            {"bmp", "image/bmp"},
+        };
+
+        string ConvertExtensionToType(const string &extension) {
+            for (auto &m : mapping_table) {
+                if (m.extension == extension) {
+                    return m.mime_type;
+                }
+            }
+            return "application/text";
+        }
+    }
+}
---
+++ mime_types.hpp
@@ -0,0 +1,20 @@
+//
+// Created by SashiRin on 2019-02-08.
+//
+
+#ifndef STHS_MIME_TYPES_HPP
+#define STHS_MIME_TYPES_HPP
+
+#include <string>
+
+namespace sashi_tiny_http {
+    using std::string;
+    using std::vector;
+    namespace mime_types {
+
+        /// Convert a file extension into a MIME type.
+        string ConvertExtensionToType(const string &extension);
+    }
+}
+
+#endif //STHS_MIME_TYPES_HPP
