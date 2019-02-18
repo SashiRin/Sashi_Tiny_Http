0eecc85ace16e274388fea5bfd49da3a8842e88d
Yukirin <ldy.nero@gmail.com>
2019-02-18 13:33:01 +0900

    Finish HTTP/1.0

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <cstdlib>
+#include "server.hpp"
 
-int main() {
-    
-    return 0;
+int main(int argc, char *argv[]) {
+    try {
+        if (argc != 4) {
+            std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
+            std::cerr << "  For IPv4, try:\n";
+            std::cerr << "    receiver 0.0.0.0 80 .\n";
+            std::cerr << "  For IPv6, try:\n";
+            std::cerr << "    receiver 0::0 80 .\n";
+            return EXIT_FAILURE;
+        }
+
+        sashi_tiny_http::Server server(argv[1], argv[2], argv[3]);
+
+        server.Run();
+    }
+    catch (std::exception &e) {
+        std::cerr << "exception: " << e.what() << std::endl;
+    }
+
+    return EXIT_SUCCESS;
 }
